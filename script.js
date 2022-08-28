75013bb4aedfee7b7eeb3d9be5bbe50d0f31b8e1
MimiB96 <109827743+MimiB96@users.noreply.github.com>
2022-08-28 19:11:50 +0100

    Added JS

---
+++ script.js
@@ -0,0 +1,81 @@
+let now = new Date();
+let day = now.getDay();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let date = now.getDate();
+let f = document.querySelector("#full-date");
+let t = document.querySelector("#full-time");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+day = days[day];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+t.innerHTML = `${hours}:${minutes}`;
+
+f.innerHTML = `${day}, ${date}, ${month}`;
+
+function cityTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#currentCity").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humid").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "25499f9fd2b3b742a2a887d3d9745f66";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${url}&appid=${apiKey}`).then(cityTemperature);
+}
+
+function citySubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city").value;
+  searchCity(city);
+}
+
+function searchCurrentLocation(position) {
+  let apiKey = "25499f9fd2b3b742a2a887d3d9745f66";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(cityTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+}
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", citySubmit);
+
+searchCity("Calgary");
+
+let currentLocationButton = document.querySelector("#currentLocationButton");
+currentLocationButton.addEventListener("click", getCurrentLocation);
